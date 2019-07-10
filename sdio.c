2ad1acd0cda4f4a263ebb980751e0ff660c7be6c
Arnauld Michelizza <arnauld.michelizza@ssi.gouv.fr>
2019-07-10 16:58:42 +0200

    Code indented.

--- sdio.c
+++ sdio.c
@@ -9,8 +9,9 @@
 #include "libc/syscall.h"
 
 #ifdef DEBUG_SDIO_INTERRUPTS
-uint32_t    log_status[20];
-int         log_count;
+uint32_t log_status[20];
+int     log_count;
+
 void init_log_status(void)
 {
     for (int i = 0; i < 20; i++) {
@@ -43,6 +44,7 @@ enum CPSM {
     CPSM_WAIT_RECEIVE,          /*At our level, the WAIT and RECEIVE states are the same */
     CPSM_PEND
 };
+
 static volatile enum CPSM cmd_path_state;
 
 enum DPSM {
@@ -53,6 +55,7 @@ enum DPSM {
     DPSM_RECEIVE,
     DPSM_READ_WAIT
 };
+
 static volatile enum DPSM data_path_state;
 
 void write_delay(void)
@@ -68,7 +71,7 @@ void write_delay(void)
 
 int8_t sdio_hw_send_cmd(uint8_t cmd, uint32_t arg, enum sdio_resp_type response)
 {
-    uint32_t    tmp = 0;
+    uint32_t tmp = 0;
 
     savestatus = 0;
 #ifdef DEBUG_SDIO_INTERRUPTS
@@ -100,7 +103,7 @@ uint32_t sdio_hw_get_short_resp(void)
 
 void sdio_hw_get_long_resp(volatile void *buffer)
 {
-    uint8_t     i;
+    uint8_t i;
     volatile uint32_t *buf = buffer;
 
     for (i = 0; i < 4; i++)
@@ -114,7 +117,8 @@ void sdio_hw_get_long_resp(volatile void *buffer)
 
 static void power_up(void)
 {
-    uint64_t    i, ii = 0;
+    uint64_t i, ii = 0;
+
     set_reg(r_CORTEX_M_SDIO_POWER, SDIO_POWER_PWRCTRL_POWER_ON,
             SDIO_POWER_PWRCTRL);
     /*
@@ -123,7 +127,6 @@ static void power_up(void)
      * After a data write, data cannot be written to this register for three SDIOCLK (48 MHz) clock
      * periods plus two PCLK2 clock periods.
      */
-
     sys_get_systick(&i, PREC_CYCLE);
     printf("debut %x\n", (uint32_t) i);
     for (; (i + 1680000) > ii; sys_get_systick(&ii, PREC_CYCLE)) ;
@@ -144,12 +147,12 @@ static void enabling_irqs(void)
                  SDIO_MASK_DATAENDIE_Msk |
                  SDIO_MASK_STBITERRIE_Msk |
                  SDIO_MASK_DBCKENDIE_Msk | SDIO_MASK_CMDACTIE_Msk);
-
 }
 
 void sdio_hw_init2()
 {
-    uint32_t    tmp;
+    uint32_t tmp;
+
     cmd_path_state = CPSM_IDLE;
     data_path_state = DPSM_IDLE;
 
@@ -165,7 +168,6 @@ void sdio_hw_init2()
     write_reg_value(r_CORTEX_M_SDIO_CLKCR, tmp | SDIO_CLKCR_CLKEN_Msk);
 
     enabling_irqs();
-
 }
 
 /*
@@ -176,28 +178,28 @@ void sdio_hw_set_clock_divisor(int32_t divisor)
     write_delay();
     if (divisor < 0) {
         set_reg_bits(r_CORTEX_M_SDIO_CLKCR, SDIO_CLKCR_BYPASS_Msk);
-    }
-    else {
+    } else {
         set_reg(r_CORTEX_M_SDIO_CLKCR, divisor, SDIO_CLKCR_CLKDIV);
     }
 }
 
 void sdio_hw_set_bus_width(uint8_t value)
 {
-    uint32_t    width;
+    uint32_t width;
+
     switch (value) {
-    case 1:
-        width = SDIO_CLKCR_WIDBUS_1WIDE_MODE;
-        break;
-    case 4:
-        width = SDIO_CLKCR_WIDBUS_4WIDE_MODE;
-        break;
-    case 8:
-        width = SDIO_CLKCR_WIDBUS_8WIDE_MODE;
-        break;
-    default:
-        printf("Set Bus Width aborted: %d is an invalid value\n", value);
-        return;
+        case 1:
+            width = SDIO_CLKCR_WIDBUS_1WIDE_MODE;
+            break;
+        case 4:
+            width = SDIO_CLKCR_WIDBUS_4WIDE_MODE;
+            break;
+        case 8:
+            width = SDIO_CLKCR_WIDBUS_8WIDE_MODE;
+            break;
+        default:
+            printf("Set Bus Width aborted: %d is an invalid value\n", value);
+            return;
     }
     printf("Changing bus width to %d wires\n", value);
     write_delay();
@@ -207,22 +209,21 @@ void sdio_hw_set_bus_width(uint8_t value)
 /*****************************************
  * DMA relative functions
  *****************************************/
-void sdio_prepare_dma(uint32_t timeout, uint32_t bytes_len ,uint32_t blocksize
-      __attribute__((unused)) )
+void sdio_prepare_dma(uint32_t timeout, uint32_t bytes_len, uint32_t blocksize
+                      __attribute__((unused)))
 {
-    uint32_t    dctrl = *r_CORTEX_M_SDIO_DCTRL;
+    uint32_t dctrl = *r_CORTEX_M_SDIO_DCTRL;
 
     write_reg_value(r_CORTEX_M_SDIO_DTIMER, timeout);
     set_reg(r_CORTEX_M_SDIO_DLEN, bytes_len, SDIO_DLEN_DATALENGTH);
 
     /* FIXME - (9<<4) means 512 bytes */
     write_reg_value(&dctrl,
-                    (9 << 4) | SDIO_DCTRL_DMAEN_Msk
-                    | SDIO_DCTRL_DTMODE_BLOCK);
+                    (9 << 4) | SDIO_DCTRL_DMAEN_Msk | SDIO_DCTRL_DTMODE_BLOCK);
     set_reg(&dctrl, SDIO_DCTRL_DTMODE_BLOCK, SDIO_DCTRL_DTMODE);
 
     write_delay();
-    *r_CORTEX_M_SDIO_DCTRL=dctrl;
+    *r_CORTEX_M_SDIO_DCTRL = dctrl;
 }
 
 void sdio_launch_dma(int i)
@@ -232,7 +233,7 @@ void sdio_launch_dma(int i)
     write_delay();
     set_reg(r_CORTEX_M_SDIO_DCTRL, 1, SDIO_DCTRL_DTEN);
 
- // write_reg_value(r_CORTEX_M_SDIO_DCTRL,(i<<SDIO_DCTRL_DTDIR_Pos)|1);
+    // write_reg_value(r_CORTEX_M_SDIO_DCTRL,(i<<SDIO_DCTRL_DTDIR_Pos)|1);
 }
 
 /* TODO: check bytes_len (block size aligned ?) */
@@ -241,7 +242,7 @@ void sdio_launch_dma(int i)
 void sdio_hw_enable_data_transfer(uint32_t timeout, uint32_t bytes_len,
                                   enum sdio_direction dir)
 {
-    uint32_t    dctrl = 0;
+    uint32_t dctrl = 0;
 
     write_reg_value(r_CORTEX_M_SDIO_DTIMER, timeout);
     set_reg(r_CORTEX_M_SDIO_DLEN, bytes_len, SDIO_DLEN_DATALENGTH);
@@ -266,14 +267,16 @@ void sdio_hw_enable_data_transfer(uint32_t timeout, uint32_t bytes_len,
 
 void sdio_hw_write_fifo(uint32_t * buf, uint32_t size)
 {
-    uint32_t    i;
+    uint32_t i;
+
     for (i = 0; i < size; i++)
         write_reg_value(r_CORTEX_M_SDIO_FIFO, buf[i]);
 }
 
 void sdio_hw_read_fifo(uint32_t * buf, uint32_t size)
 {
-    uint32_t    i;
+    uint32_t i;
+
     for (i = 0; i < size; i++)
         buf[i] = read_reg_value(r_CORTEX_M_SDIO_FIFO);
 }
@@ -293,7 +296,7 @@ volatile uint32_t *sdio_get_data_addr(void)
  *
  *****************************************/
 
-static      uint32_t(*sd_irq_handler) ();
+static  uint32_t(*sd_irq_handler) ();
 
 static void sdio_reset_static_flags()
 {
@@ -307,11 +310,11 @@ void sdio_set_irq_handler(uint32_t(*ptr) ())
 
 volatile uint32_t nbinter = 0;
 
-void SDIO_IRQHandler(uint8_t irq __UNUSED,  // IRQ number
-                     uint32_t sr,   // content of posthook.status,
-                     uint32_t dr)   // content of posthook.data)
+void SDIO_IRQHandler(uint8_t irq __UNUSED,      // IRQ number
+                     uint32_t sr,       // content of posthook.status,
+                     uint32_t dr)       // content of posthook.data)
 {
-    uint32_t __attribute__ ((unused)) mask = dr;
+    uint32_t __attribute__((unused)) mask = dr;
 
     nbinter++;
 
@@ -358,20 +361,23 @@ void sdio_set_timeout(uint32_t timeout)
 /*****************************************
  *  EXTI Handling
  *****************************************/
-volatile uint8_t SD_ejection_occured=0;
+volatile uint8_t SD_ejection_occured = 0;
 
-static void SDIO_Presence_EXTI(uint8_t irq __attribute__ ((unused)) ,
-                        uint32_t status __attribute__ ((unused)),
-                        uint32_t data __attribute__ ((unused)))
+static void SDIO_Presence_EXTI(uint8_t irq __attribute__((unused)),
+                               uint32_t status __attribute__((unused)),
+                               uint32_t data __attribute__((unused)))
 {
-  uint8_t tmp,ret;
-  ret=sys_cfg(CFG_GPIO_GET,(uint8_t)((sdio_dev_infos.gpios[SDIO_RMV].port << 4) 
-                                     + sdio_dev_infos.gpios[SDIO_RMV].pin) , &tmp);
-  if (ret != SYS_E_DONE) {
-      aprintf("Unable to read EXTI Pin value, ret %s\n", strerror(ret));
-      return;
-  }
-  SD_ejection_occured|=tmp;
+    uint8_t tmp, ret;
+
+    ret =
+        sys_cfg(CFG_GPIO_GET,
+                (uint8_t) ((sdio_dev_infos.gpios[SDIO_RMV].port << 4)
+                           + sdio_dev_infos.gpios[SDIO_RMV].pin), &tmp);
+    if (ret != SYS_E_DONE) {
+        aprintf("Unable to read EXTI Pin value, ret %s\n", strerror(ret));
+        return;
+    }
+    SD_ejection_occured |= tmp;
 };
 
 /*****************************************
@@ -379,15 +385,16 @@ static void SDIO_Presence_EXTI(uint8_t irq __attribute__ ((unused)) ,
  *****************************************/
 uint8_t sdio_early_init(void)
 {
-    uint8_t     ret = 0;
+    uint8_t ret = 0;
 
     /*******************************
      * first, SDIO device declaration
      *******************************/
-    device_t    dev;
-    memset((void*)&dev, 0, sizeof(device_t));
+    device_t dev;
 
-    int         dev_desc = 0;
+    memset((void *) &dev, 0, sizeof(device_t));
+
+    int     dev_desc = 0;
 
     /*
      * declare the SDIO device, during initialization phase
@@ -401,8 +408,8 @@ uint8_t sdio_early_init(void)
 
     /* IRQ configuration */
     dev.irqs[0].handler = SDIO_IRQHandler;
-    dev.irqs[0].irq = SDIO_IRQ;       /* starting with STACK */
-    dev.irqs[0].mode = IRQ_ISR_STANDARD;    /* if ISR force MT immediat execution, use FORCE_MAINTHREAD instead of STANDARD, and activate FISR permission */
+    dev.irqs[0].irq = SDIO_IRQ; /* starting with STACK */
+    dev.irqs[0].mode = IRQ_ISR_STANDARD;
 
     /*
      * IRQ posthook configuration
@@ -411,17 +418,17 @@ uint8_t sdio_early_init(void)
      * while the ISR has not been executed.
      * register read can be saved into 'status' and 'data' and given to the ISR in 'sr' and 'dr' argument
      */
-    dev.irqs[0].posthook.status = 0x0034;   /* STA */
+    dev.irqs[0].posthook.status = 0x0034;       /* STA */
     dev.irqs[0].posthook.data = 0x003C; /* MASK */
 
     dev.irqs[0].posthook.action[0].instr = IRQ_PH_READ;
-    dev.irqs[0].posthook.action[0].read.offset = 0x0034;    /* STA */
+    dev.irqs[0].posthook.action[0].read.offset = 0x0034;        /* STA */
 
     dev.irqs[0].posthook.action[1].instr = IRQ_PH_READ;
-    dev.irqs[0].posthook.action[1].read.offset = 0x003C;    /* MASK */
+    dev.irqs[0].posthook.action[1].read.offset = 0x003C;        /* MASK */
 
     dev.irqs[0].posthook.action[2].instr = IRQ_PH_WRITE;
-    dev.irqs[0].posthook.action[2].write.offset = 0x003C;   /* MASK */
+    dev.irqs[0].posthook.action[2].write.offset = 0x003C;       /* MASK */
     dev.irqs[0].posthook.action[2].write.value =
         SDIO_FLAG_CCRCFAIL | SDIO_FLAG_DCRCFAIL | SDIO_FLAG_CTIMEOUT |
         SDIO_FLAG_DTIMEOUT | SDIO_FLAG_TXUNDERR | SDIO_FLAG_RXOVERR |
@@ -431,17 +438,17 @@ uint8_t sdio_early_init(void)
 
     dev.irqs[0].posthook.action[3].instr = IRQ_PH_AND;
     dev.irqs[0].posthook.action[3].and.offset_dest = 0x0038;    /* ICR */
-    dev.irqs[0].posthook.action[3].and.offset_src = 0x0034; /* STA */
-    dev.irqs[0].posthook.action[3].and.mask = 0x006007ff;   /* AND action mask */
-    dev.irqs[0].posthook.action[3].and.mode = 0;    /* ICR |= STA */
+    dev.irqs[0].posthook.action[3].and.offset_src = 0x0034;     /* STA */
+    dev.irqs[0].posthook.action[3].and.mask = 0x006007ff;       /* AND action mask */
+    dev.irqs[0].posthook.action[3].and.mode = 0;        /* ICR |= STA */
 
     /* Now let's configure the GPIOs */
     dev.gpio_num = 7;
 
     /* Configuration of SDIO D[0-3] GPIO */
     dev.gpios[0].mask =
-      GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
-      GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
+        GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
+        GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
     dev.gpios[0].kref.port = sdio_dev_infos.gpios[SDIO_D0].port;
     dev.gpios[0].kref.pin = sdio_dev_infos.gpios[SDIO_D0].pin;
     dev.gpios[0].mode = GPIO_PIN_ALTERNATE_MODE;
@@ -451,8 +458,8 @@ uint8_t sdio_early_init(void)
     dev.gpios[0].afr = GPIO_AF_SDIO;
 
     dev.gpios[1].mask =
-      GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
-      GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
+        GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
+        GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
     dev.gpios[1].kref.port = sdio_dev_infos.gpios[SDIO_D1].port;
     dev.gpios[1].kref.pin = sdio_dev_infos.gpios[SDIO_D1].pin;
     dev.gpios[1].mode = GPIO_PIN_ALTERNATE_MODE;
@@ -462,8 +469,8 @@ uint8_t sdio_early_init(void)
     dev.gpios[1].afr = GPIO_AF_SDIO;
 
     dev.gpios[2].mask =
-      GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
-      GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
+        GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
+        GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
     dev.gpios[2].kref.port = sdio_dev_infos.gpios[SDIO_D2].port;
     dev.gpios[2].kref.pin = sdio_dev_infos.gpios[SDIO_D2].pin;
     dev.gpios[2].mode = GPIO_PIN_ALTERNATE_MODE;
@@ -473,8 +480,8 @@ uint8_t sdio_early_init(void)
     dev.gpios[2].afr = GPIO_AF_SDIO;
 
     dev.gpios[3].mask =
-      GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
-      GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
+        GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
+        GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
     dev.gpios[3].kref.port = sdio_dev_infos.gpios[SDIO_D3].port;
     dev.gpios[3].kref.pin = sdio_dev_infos.gpios[SDIO_D3].pin;
     dev.gpios[3].mode = GPIO_PIN_ALTERNATE_MODE;
@@ -485,8 +492,8 @@ uint8_t sdio_early_init(void)
 
     /* Configuration of SDIO_CLK */
     dev.gpios[4].mask =
-      GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
-      GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
+        GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
+        GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
     dev.gpios[4].kref.port = sdio_dev_infos.gpios[SDIO_CLK].port;
     dev.gpios[4].kref.pin = sdio_dev_infos.gpios[SDIO_CLK].pin;
     dev.gpios[4].mode = GPIO_PIN_ALTERNATE_MODE;
@@ -501,8 +508,8 @@ uint8_t sdio_early_init(void)
 
     /* configuration of SDIO_CMD  */
     dev.gpios[5].mask =
-      GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
-      GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
+        GPIO_MASK_SET_MODE | GPIO_MASK_SET_PUPD | GPIO_MASK_SET_TYPE |
+        GPIO_MASK_SET_SPEED | GPIO_MASK_SET_AFR;
     dev.gpios[5].kref.port = sdio_dev_infos.gpios[SDIO_CMD].port;
     dev.gpios[5].kref.pin = sdio_dev_infos.gpios[SDIO_CMD].pin;
     dev.gpios[5].mode = GPIO_PIN_ALTERNATE_MODE;
@@ -521,7 +528,8 @@ uint8_t sdio_early_init(void)
      *  EXTI for SD CARD removal
      *
      */
-    dev.gpios[6].mask = GPIO_MASK_SET_PUPD |GPIO_MASK_SET_MODE | GPIO_MASK_SET_EXTI;
+    dev.gpios[6].mask =
+        GPIO_MASK_SET_PUPD | GPIO_MASK_SET_MODE | GPIO_MASK_SET_EXTI;
     dev.gpios[6].kref.port = sdio_dev_infos.gpios[SDIO_RMV].port;
     dev.gpios[6].kref.pin = sdio_dev_infos.gpios[SDIO_RMV].pin;
     dev.gpios[6].pupd = GPIO_NOPULL;
@@ -540,12 +548,12 @@ uint8_t sdio_init(void)
     printf("initializing GPIO block\n");
     // Test for initial presence of a SD Card.
     uint8_t value;
-    sys_cfg(CFG_GPIO_GET,(uint8_t)((('C' - 'A') << 4) + 7) , (uint8_t*)&value);
-    SD_ejection_occured = value;
-    if(SD_ejection_occured)
-    {
-      printf("NO Card !!!\n");
 
+    sys_cfg(CFG_GPIO_GET, (uint8_t) ((('C' - 'A') << 4) + 7),
+            (uint8_t *) & value);
+    SD_ejection_occured = value;
+    if (SD_ejection_occured) {
+        printf("NO Card !!!\n");
     }
     /*
      * configure the SDIO device, once it is mapped in task memory
--- sdio.h
+++ sdio.h
@@ -4,24 +4,23 @@
 #include "libc/types.h"
 
 enum sd_error { SD_CMD_CRC_FAIL, SD_DATA_CRC_FAIL, SD_CMD_TIMEOUT,
-        SD_DATA_TIMEOUT };
+    SD_DATA_TIMEOUT
+};
+
 struct sd_callbacks {
-    void        (*response_received) (uint8_t cmd);
-    void        (*command_sent) (uint8_t cmd);
-    void        (*error) (enum sd_error);
-    void        (*fifo_empty) (uint8_t free_words);
-    void        (*fifo_full) (uint8_t available_words);
-    void        (*transfer_complete) (void);
+    void    (*response_received)(uint8_t cmd);
+    void    (*command_sent)(uint8_t cmd);
+    void    (*error)(enum sd_error);
+    void    (*fifo_empty)(uint8_t free_words);
+    void    (*fifo_full)(uint8_t available_words);
+    void    (*transfer_complete)(void);
 };
 
 /**
  * sdio_hw_init - Init the SDIO driver
  * @callbacks: List of callbacks called by the driver when an event occurs.
  */
-void        sdio_hw_init(struct sd_callbacks *callbacks);
-
-
-
+void    sdio_hw_init(struct sd_callbacks *callbacks);
 
 /**
  * sdio_hw_enable_data_transfer - Start a transfer of data
@@ -29,16 +28,15 @@ void        sdio_hw_init(struct sd_callbacks *callbacks);
  * @bytes_len: Number of bytes to transfer.
  * @dir: Direction of the transfer: OUT (write) or IN (read).
  */
-void        sdio_hw_enable_data_transfer(uint32_t timeout, uint32_t bytes_len,
-                                         enum sdio_direction dir);
-
+void    sdio_hw_enable_data_transfer(uint32_t timeout, uint32_t bytes_len,
+                                     enum sdio_direction dir);
 /**
  * sdio_hw_write_fifo - Write data in the SDIO hardware block FIFO
  * @buf: Address of the buffer which contains the data. The size of the buffer
  * must be at least @size.
  * @size: Number of words (32 bits) to write in the FIFO.
  */
-void        sdio_hw_write_fifo(uint32_t * buf, uint32_t size);
+void    sdio_hw_write_fifo(uint32_t * buf, uint32_t size);
 
 /**
  * sdio_hw_read_fifo - Read data in the SDIO hardware block FIFO
@@ -46,13 +44,11 @@ void        sdio_hw_write_fifo(uint32_t * buf, uint32_t size);
  * buffer must be at least @size.
  * @size: Number of words (32 bits) to read.
  */
-void        sdio_hw_read_fifo(uint32_t * buf, uint32_t size);
+void    sdio_hw_read_fifo(uint32_t * buf, uint32_t size);
 
+void    sdio_wait_for_cmd_completion();
+void    sdio_set_response_callback(void (*sd_response_received)(uint8_t));
+int     sdio_finished_or_error();
+void    sdio_hw_init2();
 
-void        sdio_wait_for_cmd_completion();
-void        sdio_set_response_callback(void (*sd_response_received) (uint8_t));
-
-int         sdio_finished_or_error();
-void        sdio_hw_init2();
-
-#endif                          /* !SDIO_H */
+#endif /* !SDIO_H */
